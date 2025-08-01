6d452022d279fdf05f8b7c26a72fcb15a5dcca83
Vighnesh Patil <vighneshpatil392@gmail.com>
2025-08-01 21:22:45 +0530

    Update TransactionFile.java

--- TransactionFile.java
+++ TransactionFile.java
@@ -8,18 +8,19 @@ import java.time.format.DateTimeFormatter;
 
 public class TransactionFile {
 
-    public static void logTransaction(int accountNo, String transactionType, double amount, String status) {
+    public static void logTransaction(int userId, int accountNo, String transactionType, double amount, String status) {
         try (BufferedWriter writer = new BufferedWriter(new FileWriter("transaction_history.txt", true))) {
-        	
-        	 LocalDateTime now = LocalDateTime.now();
-             DateTimeFormatter formatter = DateTimeFormatter.ofPattern("dd-MM-yyyy hh:mm");
-             String formattedDateTime = now.format(formatter);
-           
-            writer.write(accountNo + "," + transactionType + "," + amount + "," + formattedDateTime + "," + status);
-            writer.newLine();  
+            LocalDateTime now = LocalDateTime.now();
+            DateTimeFormatter formatter = DateTimeFormatter.ofPattern("dd-MM-yyyy HH:mm");
+            String formattedDateTime = now.format(formatter);
+
+            String line = userId + "," + accountNo + "," + transactionType + "," + amount + "," + formattedDateTime + "," + status;
+            writer.write(line);
+            writer.newLine();
         } catch (IOException e) {
-            System.out.println("Error while adding transaction: " + e.getMessage());
+            System.out.println("Error logging transaction: " + e.getMessage());
         }
     }
 }
+
 
